1e5b9515e1a01b695cfe2de788a59a40c77849e8
NadinKot <nadinkotlyarova@gmail.com>
2016-05-29 22:36:38 +0300

    обновление после добавления метода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -7,6 +7,33 @@ public class Distance {
 
   public static void main(String[] args) {
 
+   Point p1 = new Point(2,1);
+   Point p2 = new Point(1,1);
+
+    System.out.println ("Расстояние между точками с использованием функции  = " + dist(p1, p2));
+
+    System.out.println ("Расстояние между точками с использованием метода класса Point  = " + p1.dist( p2));
+  }
+
+  public static double dist(Point p1, Point p2) {
+
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
+  }
+}
+
+
+
+
+
+
+
+/*Код до внесения метода в классе Point, соответствует закоментированному кусочку класса Point
+
+public class Distance {
+
+  public static void main(String[] args) {
+
     Point p1 = new Point();
     p1.x = 2;
     p1.y = -1;
@@ -14,6 +41,10 @@ public class Distance {
     Point p2 = new Point();
     p2.x = 1;
     p2.y = 0;
+
+   //// Point p1 = new Point(2,2);
+    ////Point p2 = new Point(1,1);
+
 
     System.out.println ("Расстояние между точками  = " + dist(p1, p2));
   }
@@ -23,4 +54,4 @@ public class Distance {
     return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
 
   }
-}
+}*/
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,4 +6,38 @@ package ru.stqa.pft.sandbox;
 public class Point {
   public double x;
   public double y;
+
+  public Point(double m, double n) {
+    this.x = m;
+    this.y = n;
+  }
+
+  public double dist( Point p2) {
+
+    return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+/*Код до внесения метода, в классе Distance есть соответствующий закоментированный кусочек
+
+public class Point {
+  public double x;
+  public double y;
+
+    ///public Point(double m, double n) {
+    ///this.x = m;
+    ///this.y = n;
+
+  }
+}*/
