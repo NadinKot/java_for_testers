8ccc60a3d619a277ed8d6c642b910777bc938b4f
NadinKot <nadinkotlyarova@gmail.com>
2016-05-29 23:45:39 +0300

    Тест для проверки расстояния между точками (что оно больше 0)

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistTest.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Nadin_Kot on 29.05.2016.
+ */
+
+public class DistTest {
+
+  @Test
+  public void testDist(){
+
+    Point p1 = new Point(2,1);
+    Point p2 = new Point(1,1);
+    //assert p1.dist(p2) >=0;
+    Assert.assertTrue(p1.dist(p2) >=0);
+  }
+
+}
