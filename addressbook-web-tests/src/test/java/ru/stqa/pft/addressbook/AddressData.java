1744db0f7e88a93ef48293c54eae1a9f7ee05753
NadinKot <nadinkotlyarova@gmail.com>
2016-06-03 00:35:18 +0300

    Тест на создание нового адреса

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddressData.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook;
+
+public class AddressData {
+  private final String firstname;
+  private final String lastname;
+  private final String nickname;
+  private final String address;
+  private final String mobile;
+  private final String email;
+  private final String secondAddress;
+
+  public AddressData(String firstname, String lastname, String nickname, String address, String mobile, String email, String secondAddress) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.address = address;
+    this.mobile = mobile;
+    this.email = email;
+    this.secondAddress = secondAddress;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getSecondAddress() {
+    return secondAddress;
+  }
+}
